41440b2e99b1af9ec04decb007460176cce48373
Athena <az2679@nyu.edu>
2024-02-11 15:49:30 -0500

    two body problem

--- src/forces/mover.js
+++ src/forces/mover.js
@@ -1,10 +1,19 @@
 class Mover {
-  constructor(x, y, m) {
+  constructor(x, y, vx, vy, m) {
     this.pos = createVector(x, y);
-    this.vel = p5.Vector.random2D();
+    this.vel = createVector(vx, vy);
     this.acc = createVector(0, 0);
     this.mass = m;
     this.r = sqrt(this.mass) * 2;
+  }
+
+  attract(mover) {
+    let force = p5.Vector.sub(this.pos, mover.pos);
+    let distanceSq = constrain(force.magSq(), 100, 1000);
+    let G = 25;
+    let strength = (G * (this.mass * mover.mass)) / distanceSq;
+    force.setMag(strength);
+    mover.applyForce(force);
   }
 
   drag(c) {
--- src/forces/sketch.js
+++ src/forces/sketch.js
@@ -1,30 +1,26 @@
 let movers = [];
-let attractor;
 
 function setup() {
-  createCanvas(600, 600);
-  for (let i = 0; i < 10; i++) {
-    let x = random(width);
-    let y = random(height);
-    let m = random(50, 150);
-    movers[i] = new Mover(x, y, m);
-  }
-  attractor = new Attractor(width / 2, height / 2, 100);
+  createCanvas(400, 400);
+  // for (let i = 0; i < 2; i++) {
+  //   let x = random(width);
+  //   let y = random(height);
+  //   let m = random(50, 150);
+  //   movers[i] = new Mover(x, y, m);
+  // }
+  movers[0] = new Mover(300, 200, 0, 5, 10);
+  movers[1] = new Mover(100, 200, 0, -5, 10);
+
+  background(0);
 }
 
 function draw() {
-  background(0);
+  // background(0);
+  movers[0].attract(movers[1]);
+  movers[1].attract(movers[0]);
 
   for (let mover of movers) {
     mover.show();
     mover.update();
-    attractor.attract(mover);
   }
-
-  if (mouseIsPressed) {
-    attractor.pos.x = mouseX;
-    attractor.pos.y = mouseY;
-  }
-
-  attractor.show();
 }
