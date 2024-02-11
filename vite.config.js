5cbc0189c027cbd8fdcc108a5c249d167e262492
Athena <az2679@nyu.edu>
2024-02-11 11:29:18 -0500

    debug deploy

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [react(), resolve()],
-  root: resolve(__dirname, 'src'),
+  docs: resolve(__dirname, 'src'),
   publicDir: resolve(__dirname, 'public'),
   build: {
     outDir: '../docs',
